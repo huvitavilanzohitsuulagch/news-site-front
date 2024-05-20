38ad5067de485ac4ce2032238498cca03644a214
barsbold-coding <barsbold.coding@gmail.com>
2024-05-21 00:16:59 +0800

    feat: added constants

---
+++ src/constants.js
@@ -0,0 +1,11 @@
+export const BASE_URL = 'http://localhost:8080/api/';
+
+export const LOGIN_STATUS = {
+  PENDING: 'pending',
+  LOGGED_IN: 'logged-in',
+  LOGGED_OUT: 'logged-out',
+};
+
+export const API_PATHS = {
+  login: 'auth/sign-in',
+}
